93c758a60300f5853f46b5d81a6ad1f662018901
Pete Castellano <peter.a.castellano@outlook.com>
2026-07-09 13:15:14 -0500

    Add return flight rebooking fare difference to New Orleans report
    Return leg changed from MSY-STL-OMA to MSY-DAL-OMA (WN4419/WN1374);
    $105.01 fare difference charged to Visa -2674 on 7/8/26.

--- travel/trips/build_new_orleans_expense_report_unifocus.ps1
+++ travel/trips/build_new_orleans_expense_report_unifocus.ps1
@@ -22,7 +22,8 @@ $yellow    = [long]0xFFFF99
 $knownRows = @(
     @("7/6/2026", 545.19, 1.00, "PERSONAL", "SW round-trip OMA-MSY (CVMCLW): outbound OMA-DEN-MSY (WN2060/WN1643, rebooked from OMA-DAL-MSY - mechanical delay); return MSY-STL-OMA 7/9 (WN3021/WN4912) - card charge portion, Visa -2674", "Air Fare", "HM Alpha - InterContinental New Orleans", ""),
     @("7/6/2026", 316.20, 1.00, "PERSONAL", "SW round-trip OMA-MSY (CVMCLW) - SW travel credit applied", "Air Fare", "HM Alpha - InterContinental New Orleans", ""),
-    @("7/6/2026", 31.08, 1.00, "PERSONAL", "HMSHost/Great Divide C, Denver Airport (DEN layover, mechanical delay) - taco barbacoa, soda, tip", "Lunch", "HM Alpha - InterContinental New Orleans", "")
+    @("7/6/2026", 31.08, 1.00, "PERSONAL", "HMSHost/Great Divide C, Denver Airport (DEN layover, mechanical delay) - taco barbacoa, soda, tip", "Lunch", "HM Alpha - InterContinental New Orleans", ""),
+    @("7/9/2026", 105.01, 1.00, "PERSONAL", "SW fare difference - return leg rebooked MSY-DAL-OMA (WN4419/WN1374) replacing MSY-STL-OMA, Visa -2674", "Air Fare", "HM Alpha - InterContinental New Orleans", "")
 )
 $blankRowCount = 14
 
